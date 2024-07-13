de63d7406aa9b897086e28af8fad3729ece579e2
pandistic-zain <zainsaith7@gmail.com>
2024-07-13 15:32:21 +0500

    "Updated DataTrans component: reformatted code, added whitespace, and changed button element syntax."

--- tutorial/src/Components/DataTrans.jsx
+++ tutorial/src/Components/DataTrans.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 
 export default function DataTrans(p) {
-
-const quote = "Q fer Pehndi A Naw Thaak!"
+  const quote = "Q fer Pehndi A Naw Thaak!";
   return (
     <>
       <h1>Data Transfer From Child Component</h1>
-        <h1>Showing data {p.data}</h1>
-        <button onClick={()=>{p.data(quote)}}>Click Me</button>
+      <button
+        onClick={() => {
+          p.data(quote);
+        }}
+      >
+        Click Me
+      </button>
     </>
   );
 }
