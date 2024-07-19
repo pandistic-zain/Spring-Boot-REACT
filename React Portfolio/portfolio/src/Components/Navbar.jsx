d8b5f9cd3fea87c54c1e3d3f22bf504ce774bc0f
pandistic-zain <zainsaith7@gmail.com>
2024-07-19 18:56:25 +0500

    "Added state and effect to Navbar component to track active link and scroll position"

--- React Portfolio/portfolio/src/Components/Navbar.jsx
+++ React Portfolio/portfolio/src/Components/Navbar.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 export default function NavBar() {
+  const[activeLink  ,setActiveLink] = useState("home");
+  const[scrolled  ,setScrolled] = useState(false);
+  useEffect(()=>{
+    const onScroll = () =>{
+      if (window.scrollY>50){
+        setScrolled(true);
+      }else{
+        setScrolled(false);
+      }
+  }
+  window.addEventListener("scroll",onScroll);
+  return ()=>{
+    window.removeEventListener("scroll",onScroll);
+  }
+},[])
+function onUpdateActiveLink (value) {
+    setActiveLink(value);
+  }
   return (
     <>
       <Navbar expand="lg" className="bg-body-tertiary">
@@ -16,9 +34,9 @@ export default function NavBar() {
           </Navbar.Toggle>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#skilss">Skills</Nav.Link>
-              <Nav.Link href="#projects">Projects</Nav.Link>
+              <Nav.Link href="#home" className={activeLink==="home"?'activenavbar-link' : 'navbar-link' }onClick={()=> onUpdateActiveLink("home")}>Home</Nav.Link>
+              <Nav.Link href="#skills"className={activeLink==="skills"?'activenavbar-link' : 'navbar-link'} onClick={()=> onUpdateActiveLink("skills")}>Skills</Nav.Link>
+              <Nav.Link href="#projects"className={activeLink==="Projects"?'activenavbar-link' : 'navbar-link'} onClick={()=> onUpdateActiveLink("projects")}>Projects</Nav.Link>
             </Nav>
             <span className="Navbar-text">
               <div className="socia-icons">
