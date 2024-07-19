a5b8b1c0766d3942638f2b5177c0950ace6ac75a
pandistic-zain <zainsaith7@gmail.com>
2024-07-19 18:28:04 +0500

    "Updated Navbar component with new logo, social icons, and contact button"

--- React Portfolio/portfolio/src/Components/Navbar.jsx
+++ React Portfolio/portfolio/src/Components/Navbar.jsx
@@ -1,34 +1,52 @@
-import React from 'react'
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 export default function NavBar() {
   return (
     <>
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img src={""} alt="Logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <span className="navbar-toggler-icon"></span>
+          </Navbar.Toggle>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#skilss">Skills</Nav.Link>
+              <Nav.Link href="#projects">Projects</Nav.Link>
+            </Nav>
+            <span className="Navbar-text">
+              <div className="socia-icons">
+                <a href="#">
+                  {" "}
+                  <img src={""} alt="Icon1" />
+                </a>
+                <a href="#">
+                  {" "}
+                  <img src={""} alt="Icon3" />
+                </a>
+                <a href="#">
+                  {" "}
+                  <img src={""} alt="Icon2" />
+                </a>
+              </div>
+              <button
+                className="contact-me"
+                onClick={() => {
+                  console.log("Contact Me Form");
+                }}
+              >
+                <span>Lets Connect</span>
+              </button>
+            </span>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
-  )
+  );
 }
