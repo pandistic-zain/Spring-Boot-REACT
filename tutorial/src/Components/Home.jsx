3a6dbdecccb73241611af2f941c107fff3a45b1e
pandistic-zain <zainsaith7@gmail.com>
2024-07-20 18:14:30 +0500

    "Fixed relative path imports in Home.jsx"

--- tutorial/src/Components/Home.jsx
+++ tutorial/src/Components/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./App.css";
-import States from ".States.jsx";
-import Props from ".Props.jsx";
-import HooksUE from ".HooksUE.jsx";
-import HookUM from ".HookUM.jsx";
+import States from "./States.jsx";
+import Props from "./Props.jsx";
+import HooksUE from "./HooksUE.jsx";
+import HookUM from "./HookUM.jsx";
 export default function Home() {
   return (
     <>
