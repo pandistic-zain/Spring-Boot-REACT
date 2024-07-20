c832ab461a338beb7d574ab288557c25d7f85ac2
pandistic-zain <zainsaith7@gmail.com>
2024-07-21 03:07:33 +0500

    "Added Banner component to App.jsx"

--- React Portfolio/portfolio/src/App.jsx
+++ React Portfolio/portfolio/src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
 import NavBar from "./Components/NavBar.jsx";
+import Banner from "./Components/Banner.jsx";
+
 function App() {
   return (
     <>
     <NavBar />
+    <Banner />
     </>
   );
 }
