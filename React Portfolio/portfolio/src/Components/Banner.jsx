f044c55da996a359c3f9d8f964e61c63ef6bfa9e
pandistic-zain <zainsaith7@gmail.com>
2024-07-21 03:28:54 +0500

    "Added react-bootstrap components and HTML structure to Banner component"

--- React Portfolio/portfolio/src/Components/Banner.jsx
+++ React Portfolio/portfolio/src/Components/Banner.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import '../App.css'
+import { Container ,Row, Col } from 'react-bootstrap';
 export default function Banner() {
   return (
     <>
-      
+    <section className='banner' id = 'home'>
+      <Container>
+        <Row className="align-items-center">
+          <Col xs = {12} >
+          <h1 className="display-4">Welcome to my Portfolio</h1>
+          </Col>
+        </Row>
+
+      </Container>
+    </section>
     </>
   );
 }
