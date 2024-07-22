090f9ec4d59f88647d4c264cd113bb0540fd433f
pandistic-zain <zainsaith7@gmail.com>
2024-07-23 04:15:01 +0500

    "Added Bootstrap dependency and updated App.css, removed App.js, and modified index.js to import Bootstrap CSS"

---
+++ Employee Mangement System/em-project-frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import NavBar from './Components/NavBar';
+import Loader from './Components/Loader';
+
+function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate an asynchronous operation
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000); // Adjust the delay as needed
+  }, []);
+
+  return (
+    <>
+      {loading ? <Loader /> : (
+        <>
+          <NavBar />
+          <h1>Hello Employee Management System</h1>
+        </>
+      )}
+    </>
+  );
+}
+
+export default App;
