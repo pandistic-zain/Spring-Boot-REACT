8a73ac8e5647f8a38e5655f7dacf6850b66c925d
Zain <zainsaith7@gmail.com>
2024-07-24 05:29:53 +0500

    "Updated AddEmployee and UpdateEmployee components to use Edit-AddEmployee.css, removed AddEmployee.css, and made changes to UpdateEmployee component's JSX and CSS."

--- Employee Mangement System/em-project-frontend/src/Components/UpdateEmployee.jsx
+++ Employee Mangement System/em-project-frontend/src/Components/UpdateEmployee.jsx
@@ -2,11 +2,32 @@ import React from 'react'
 
 const UpdateEmployee = () => {
   return (
-    <>
-    <h1>Update Employee</h1>
-      
-    </>
-  )
+    <div className="login-container">
+      <div className="login-box">
+        <h1>Add Employee</h1>
+        <form>
+          <div className="user-box">
+            <input type="text" name="" required="" />
+            <label>Name</label>
+          </div>
+          <div className="user-box">
+            <input type="text" name="" required="" />
+            <label>Email</label>
+          </div>
+          <div className="user-box">
+            <input type="text" name="" required="" />
+            <label>Phone</label>
+          </div>
+          <center>
+            <a href="/">
+              ADD Employee
+              <span></span>
+            </a>
+          </center>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default UpdateEmployee
