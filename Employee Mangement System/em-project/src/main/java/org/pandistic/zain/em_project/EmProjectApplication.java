f3c54b7b63f36c7a92c3d16d28aff50218e4994d
pandistic-zain <zainsaith7@gmail.com>
2024-07-24 16:01:52 +0500

    "Updated VSCode settings and pom.xml file, reformatted code in EmProjectApplication.java"

--- Employee Mangement System/em-project/src/main/java/org/pandistic/zain/em_project/EmProjectApplication.java
+++ Employee Mangement System/em-project/src/main/java/org/pandistic/zain/em_project/EmProjectApplication.java
@@ -3,11 +3,12 @@ package org.pandistic.zain.em_project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-@EnableJpaRepositories
+
 @SpringBootApplication(scanBasePackages = "org.pandistic.zain.em_project")
+@EnableJpaRepositories
 public class EmProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EmProjectApplication.class, args);
+    }
 }
