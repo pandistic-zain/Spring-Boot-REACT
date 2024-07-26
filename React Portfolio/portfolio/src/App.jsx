0eda73f82d5dbef23432ba15d8a2bb188c69d84b
pandistic-zain <zainsaith7@gmail.com>
2024-07-27 03:22:06 +0500

    "Removed border-color style from h6 in App.css and added Project component to App.jsx"

--- React Portfolio/portfolio/src/App.jsx
+++ React Portfolio/portfolio/src/App.jsx
@@ -3,12 +3,14 @@ import NavBar from "./Components/NavBar.jsx";
 import Banner from "./Components/Banner.jsx";
 import Home from "./Components/Home.jsx";
 import Skills from "./Components/Skills.jsx";
+import Project from "./Components/Project.jsx";
 
 function App() {
   return (
     <>
     <Home />
     <Skills/>
+    <Project />
     </>
   );
 }
