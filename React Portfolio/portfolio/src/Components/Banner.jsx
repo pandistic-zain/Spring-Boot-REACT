629d28c385d6aeb8e2ddccbe175978f8bf7fb66c
pandistic-zain <zainsaith7@gmail.com>
2024-07-26 18:34:27 +0500

    "Added react-bootstrap-icons, updated package.json and package-lock.json, modified App.css and added new Banner component with animation and styling"

--- React Portfolio/portfolio/src/Components/Banner.jsx
+++ React Portfolio/portfolio/src/Components/Banner.jsx
@@ -1,20 +1,78 @@
-import React from 'react'
-import '../App.css'
-import { Container ,Row, Col } from 'react-bootstrap';
+import { React, useState, useEffect } from "react";
+import "../App.css";
+import { Container, Row, Col } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../Assets/images/header-img.svg";
 export default function Banner() {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState("");
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = [
+    "Web Developer",
+    "Web Designer",
+    "UI/UX Designer",
+    "Frontend Developer",
+    "Backend Developer",
+  ];
+  const period = 2000;
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
+  function tick() {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+    setText(updatedText);
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    } 
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    }
+    else if(isDeleting && updatedText === '') {
+      setDelta(500);
+      setIsDeleting(false);
+      setLoopNum(loopNum+1);
+    }
+  }
+
   return (
     <>
-    <section className='banner' id = 'home'>
-      <Container>
-        <Row className="align-items-center">
-          <Col xs = {12} >
-          <h1 className="display-4">Welcome to my Portfolio</h1>
-          </Col>
-        </Row>
-
-      </Container>
-    </section>
+      <section className="banner" id="home">
+        <Container>
+          <Row className="align-items-center">
+            <Col xs={12} md={6} xl={7}>
+              <span className="tagline">Welcome To My Portfolio</span>
+              <h1>
+                {" "}
+                {`Hi I'm `} <span className="wrap"> {text}</span>
+              </h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Labore, accusamus? Illo, dolorum molestiae iste nam sunt
+                quaerat, autem, obcaecati dolor vel sit in ea? Molestias aliquam
+                beatae placeat consectetur voluptas.
+              </p>
+              <button onClick={() => console.log("Lets Connect")}>
+                Let's Connect <ArrowRightCircle size={25} />{" "}
+              </button>
+            </Col>
+            <Col xs={12} md={6} xl={5}>
+              <img src={headerImg} alt="Header Image" />
+            </Col>
+          </Row>
+        </Container>
+      </section>
     </>
   );
 }
-
