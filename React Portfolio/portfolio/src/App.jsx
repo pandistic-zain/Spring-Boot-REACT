f128a75eea976c3eac428bdeddb4b52311501a49
pandistic-zain <zainsaith7@gmail.com>
2024-07-27 06:20:46 +0500

    "Removed imports for NavBar and Banner components in App.jsx"

--- React Portfolio/portfolio/src/App.jsx
+++ React Portfolio/portfolio/src/App.jsx
@@ -1,6 +1,4 @@
 import "./App.css";
-import NavBar from "./Components/NavBar.jsx";
-import Banner from "./Components/Banner.jsx";
 import Home from "./Components/Home.jsx";
 import Skills from "./Components/Skills.jsx";
 import Projects from "./Components/Projects.jsx";
