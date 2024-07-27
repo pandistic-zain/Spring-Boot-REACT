40a728bb1843bda772c4943b3627210b8a5741eb
pandistic-zain <zainsaith7@gmail.com>
2024-07-28 01:02:10 +0500

    "Implemented GetFeedback method and added dependencies for FeedbackRepository and BeanUtils"

--- React Portfolio/portfolio-backend/src/main/java/org/portfolio/backend/zain/portfolio_backend/Services/fbServicesImplementation.java
+++ React Portfolio/portfolio-backend/src/main/java/org/portfolio/backend/zain/portfolio_backend/Services/fbServicesImplementation.java
@@ -1,16 +1,31 @@
 package org.portfolio.backend.zain.portfolio_backend.Services;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.portfolio.backend.zain.portfolio_backend.Controller.Feedback;
+import org.portfolio.backend.zain.portfolio_backend.DAO.Entity.FeedbackEntity;
+import org.portfolio.backend.zain.portfolio_backend.DAO.Repository.FeedbackRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 public class fbServicesImplementation implements fbServices{
+    @Autowired
+    FeedbackRepository feedbackRepository;
     @Override
     public String postFeedback(Feedback feedback){
         return "null";
     }
     @Override
     public List<Feedback> GetFeedback() {
-        throw new UnsupportedOperationException("Unimplemented method 'GetFeedback'");
+        List<FeedbackEntity> feedbackentity = feedbackRepository.findAll();
+        List<Feedback> feedback = new ArrayList<>();
+        for (FeedbackEntity fbEntity : feedbackentity){
+            Feedback fb = new Feedback();
+            BeanUtils.copyProperties(fbEntity, fb);
+            feedback.add(fb);       
     }
+        return feedback;
+}
 }
