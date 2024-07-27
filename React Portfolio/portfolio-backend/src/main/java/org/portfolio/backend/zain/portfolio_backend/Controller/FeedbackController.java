a4a028f9867a0838e3fef7d138ca0d71ac4f1488
pandistic-zain <zainsaith7@gmail.com>
2024-07-28 01:07:30 +0500

    "Added Autowired fbServices and implemented methods in FeedbackController"

--- React Portfolio/portfolio-backend/src/main/java/org/portfolio/backend/zain/portfolio_backend/Controller/FeedbackController.java
+++ React Portfolio/portfolio-backend/src/main/java/org/portfolio/backend/zain/portfolio_backend/Controller/FeedbackController.java
@@ -2,6 +2,8 @@ package org.portfolio.backend.zain.portfolio_backend.Controller;
 
 import java.util.List;
 
+import org.portfolio.backend.zain.portfolio_backend.Services.fbServices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,15 +15,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 @CrossOrigin("http://localhost:3000")
 public class FeedbackController {
+    @Autowired
+    fbServices FbService;
     @GetMapping("/letsconnect")
     public List<Feedback> getAllFeedbacks() {
-        return null;
+        return FbService.GetFeedback();
     }
     @PostMapping("/letsconnect")
-    public String postFeedback(@RequestBody Feedback entity) {
-        return "Done";
+    public String postFeedback(@RequestBody Feedback fb){
+        return FbService.postFeedback(fb);
     }
-    
-    
-    
 }
