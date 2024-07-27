b6c77a85b9e94bac182d86ec914269a3b42b511f
pandistic-zain <zainsaith7@gmail.com>
2024-07-27 17:19:50 +0500

    "Updated package dependencies, modified App.css and App.jsx files, added react-router-dom, and updated Footer.jsx and index.js files"

--- React Portfolio/portfolio/src/App.jsx
+++ React Portfolio/portfolio/src/App.jsx
@@ -3,14 +3,16 @@ import Home from "./Components/Home.jsx";
 import Skills from "./Components/Skills.jsx";
 import Projects from "./Components/Projects.jsx";
 import Footer from "./Components/Footer.jsx";
+import { Routes, Route } from "react-router-dom";
+import FrontPage from "./Components/FrontPage.jsx";
 
 function App() {
   return (
     <>
-    <Home />
-    <Skills/>
-    <Projects />
-    <Footer />
+    <Routes>
+      <Route exact path="/" element={<FrontPage/>}></Route>
+    </Routes>
+   
     </>
   );
 }
---
+++ React Portfolio/portfolio/src/Components/FrontPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Home from "./Home.jsx";
+import Skills from "./Skills.jsx";
+import Projects from "./Projects.jsx";
+import Footer from "./Footer.jsx";
+const FrontPage = () => {
+  return (
+    <>
+    <Home />
+    <Skills/>
+    <Projects />
+    <Footer />
+    </>
+  )
+}
+
+export default FrontPage
